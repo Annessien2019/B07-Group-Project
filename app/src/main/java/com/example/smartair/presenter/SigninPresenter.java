257ff1b15b70fe42f8932c61671bd97e12b3f799
Nihar <nihar.samant1812@gmail.com>
2025-11-25 03:17:07 -0500

    Implemented presenter and callback interface

--- app/src/main/java/com/example/smartair/presenter/SigninPresenter.java
+++ app/src/main/java/com/example/smartair/presenter/SigninPresenter.java
@@ -4,15 +4,24 @@ import android.widget.Toast;
 
 import com.example.smartair.model.SigninModel;
 import com.example.smartair.view.SigninFragmentView;
+import com.google.firebase.auth.FirebaseUser;
 
-public class SigninPresenter {
+public class SigninPresenter implements SignInCallback{
 
     private SigninFragmentView view;
     private SigninModel model;
-
+    private FirebaseUser currentUser;
     public SigninPresenter(SigninFragmentView view) {
-        this.model = new SigninModel();
         this.view = view;
+    }
+    @Override
+    public void onSignInSuccess(FirebaseUser user){
+        this.currentUser = user;
+        Toast.makeText(view.getContext(), "Logging in with " + String.valueOf(user.getUid()) , Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onSignInFailure(Exception e) {
+        Toast.makeText(view.getContext(), "User doesn't exist ", Toast.LENGTH_SHORT).show();
     }
 
     public void onSignInClick(String email, String password) {
@@ -20,7 +29,11 @@ public class SigninPresenter {
             Toast.makeText(view.getContext(), "Please fill out all the fields", Toast.LENGTH_SHORT).show();
             return;
         }
-        Toast.makeText(view.getContext(), "Signing In", Toast.LENGTH_SHORT).show();
-        System.out.println("Signing into \nEmail: " + email + "\nPassword: " + password);
+        model = new SigninModel(email, password);
+        model.signInAttempt(this);
+
     }
+
 }
+
+
