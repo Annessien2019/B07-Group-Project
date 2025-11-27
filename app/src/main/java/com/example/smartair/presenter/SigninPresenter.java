d995099553c85d8b36ac1fe029f123eaad69e346
Nihar <nihar.samant1812@gmail.com>
2025-11-27 14:34:03 -0500

    Extract userID and role from dB to Presenter

--- app/src/main/java/com/example/smartair/presenter/SigninPresenter.java
+++ app/src/main/java/com/example/smartair/presenter/SigninPresenter.java
@@ -1,33 +1,46 @@
 package com.example.smartair.presenter;
 
-import android.widget.Toast;
-
+import android.util.Pair;
 import com.example.smartair.model.SigninModel;
 import com.example.smartair.view.SigninFragmentView;
 import com.google.firebase.auth.FirebaseUser;
 
-public class SigninPresenter implements CallbackAbility{
-
-    private SigninFragmentView view;
-    private SigninModel model;
+public class SigninPresenter implements CallbackAuth, CallbackReadDB{
+    private final SigninFragmentView view;
+    private final SigninModel model;
     private FirebaseUser currentUser;
+    private Pair<String, String> userData;
+
     public SigninPresenter(SigninFragmentView view) {
         this.view = view;
         this.model = new SigninModel();
     }
-    @Override
-    public void onSuccess(FirebaseUser user){
-        this.currentUser = user;
-        this.view.signInSuccessToast(String.valueOf(user.getUid()));
-    }
-    @Override
-    public void onFailure(Exception e) {
-        this.view.signInFailureToast(e.getMessage());
-    }
-
     public void onSignInClick(String email, String password) {
+        if(email.isEmpty() || password.isEmpty()){
+            this.view.makeToast("Please fill the fields");
+            return;
+        }
         this.model.getData(email, password);
         model.signInAttempt(this);
+    }
+
+    @Override
+    public void onAuthSuccess(FirebaseUser user){
+        this.currentUser = user;
+        this.model.sendUserDataAsync(this);
+    }
+    @Override
+    public void onDBreadSuccess(Pair<String,String> userData){
+        this.userData = userData;
+        this.view.makeToast( "Logging in as a " + userData.second);
+    }
+    @Override
+    public void onAuthFailure(Exception e) {
+        this.view.makeToast(e.getMessage());
+    }
+    @Override
+    public void onDBreadFailure(Exception e) {
+        this.view.makeToast(e.getMessage());
     }
 
 }
