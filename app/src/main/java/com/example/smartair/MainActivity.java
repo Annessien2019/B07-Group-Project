7e26f1bd913f57a473b9141b4dc86526ecca461e
Nihar <nihar.samant1812@gmail.com>
2025-11-28 03:47:26 -0500

    Added Fragment Handling in Main Activity

--- app/src/main/java/com/example/smartair/MainActivity.java
+++ app/src/main/java/com/example/smartair/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.smartair;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.smartair.view.DoseCheckView;
+import com.example.smartair.view.FragmentListener;
 import com.example.smartair.view.InventoryFragmentView;
 import com.example.smartair.view.ChildrenMainView;
+import com.example.smartair.view.InventoryLogView;
 import com.example.smartair.view.ParentMainView;
 import com.example.smartair.view.ProviderMainView;
 import com.example.smartair.view.MedicineLogsFragmentView;
@@ -15,18 +20,52 @@ import com.example.smartair.view.RecoveryView;
 import com.example.smartair.view.SigninFragmentView;
 import com.example.smartair.view.SignupFragmentView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentListener {
+
+    FragmentManager manager;
+    String fragName;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.manager = getSupportFragmentManager();
+        this.fragName = null;
 
+        setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.main_fragment_container, new DoseCheckView())
+            manager.beginTransaction()
+                    .replace(R.id.main_fragment_container, new SigninFragmentView())
                     .commit();
         }
     }
+    @Override
+    public void showToast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+    @Override
+    public void onFragmentAction(Fragment nextFragment, Bundle dataBundle, boolean stackState){
+        nextFragment.setArguments(dataBundle);
+        if(stackState) {
+            this.manager.beginTransaction()
+                    .setReorderingAllowed(true)
+                    .replace(R.id.main_fragment_container, nextFragment)
+                    .addToBackStack(null)
+                    .commit();
+            return;
+        }
+        this.manager.beginTransaction()
+                .setReorderingAllowed(true)
+                .replace(R.id.main_fragment_container, nextFragment)
+                .commit();
+    }
+    @Override
+    public void clearFragment(){
+        Fragment fragmentToRemove = manager.findFragmentById(R.id.main_fragment_container);
+        if (fragmentToRemove != null) {
+            manager.beginTransaction()
+                    .remove(fragmentToRemove)
+                    .commit();
+        }
+    }
+
 }
