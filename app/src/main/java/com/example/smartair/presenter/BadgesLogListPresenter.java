54df9333839cc60218c4f58f39a64251c0944875
Noah Miguel Perez <noahm.p.in2017@gmail.com>
2025-11-30 13:37:14 -0500

    fix: minor tweaks to badge display formatting

--- app/src/main/java/com/example/smartair/presenter/BadgesLogListPresenter.java
+++ app/src/main/java/com/example/smartair/presenter/BadgesLogListPresenter.java
@@ -28,40 +28,6 @@ public class BadgesLogListPresenter implements LogListPresenter {
     public void queryLogs() {
         logData = new ArrayList<>();
 
-        // NOTE: These Badges are manually set by the developers
-        logData.add(new BadgeLog("First perfect controller week",
-                v -> {
-                                        return false;
-                                    }));
-        logData.add(new BadgeLog("10 high-quality technique sessions",
-                v -> {
-                                        return false;
-                                    }));
-        logData.add(new BadgeLog("Low-rescue month",
-                                    v -> {
-                                        return false;
-                                    }));
-        logData.add(new BadgeLog("Reach 10 controller logs",
-                                v-> {
-                                    int numController = 0;
-                                    for (MedicineLog log : v) {
-                                        if (log.getMedicineType().equals("Control")) {
-                                            numController++;
-                                        }
-                                    }
-                                    return numController >= 10;
-                                }));
-        logData.add(new BadgeLog("Reach 10 rescue logs",
-                v-> {
-                    int numRescue = 0;
-                    for (MedicineLog log : v) {
-                        if (log.getMedicineType().equals("Rescue")) {
-                            numRescue++;
-                        }
-                    }
-                    return numRescue >= 10;
-                }));
-
         // TODO: IMPLEMENT MODEL TO QUERY *MEDICINE LOG* DATA
         medicineLogs = new ArrayList<>();
         medicineLogs.add(new MedicineLog("Rescue", 2, new Date(2024, 2, 20)));
@@ -78,11 +44,46 @@ public class BadgesLogListPresenter implements LogListPresenter {
         medicineLogs.add(new MedicineLog("Control", 2, new Date(2024, 2, 20)));
         medicineLogs.add(new MedicineLog("Control", 2, new Date(2024, 2, 20)));
         medicineLogs.add(new MedicineLog("Control", 2, new Date(2024, 2, 20)));
+
+        // NOTE: These Badges are manually set by the developers
+        logData.add(new BadgeLog("First perfect controller week",
+                v -> {
+                                        return false;
+                                    }));
+        logData.add(new BadgeLog("10 high-quality technique sessions",
+                v -> {
+                                        return false;
+                                    }));
+        logData.add(new BadgeLog("Low-rescue month",
+                                    v -> {
+                                        return false;
+                                    }));
+        logData.add(new BadgeLog("Reach 10 controller logs",
+                v-> {
+                    int numController = 0;
+                    for (MedicineLog log : v) {
+                        if (log.getMedicineType().equals("Control")) {
+                            numController++;
+                        }
+                    }
+                    return numController >= 10;
+                }));
+        logData.add(new BadgeLog("Reach 15 controller logs",
+                v-> {
+                    int numController = 0;
+                    for (MedicineLog log : v) {
+                        if (log.getMedicineType().equals("Control")) {
+                            numController++;
+                        }
+                    }
+                    return numController >= 50;
+                }));
     }
 
     @Override
     public void loadLogs() {
         ArrayList<BadgeLogFragment> fragments = new ArrayList<>();
+        BadgeLogFragment[] logs;
         BadgeLogFragment fragment;
         int bgColorId;
 
@@ -94,7 +95,7 @@ public class BadgesLogListPresenter implements LogListPresenter {
             fragments.add(fragment);
         }
 
-        BadgeLogFragment[] logs = new BadgeLogFragment[fragments.size()];
+        logs = new BadgeLogFragment[fragments.size()];
         fragments.toArray(logs);
         view.setLogs(logs);
     }
