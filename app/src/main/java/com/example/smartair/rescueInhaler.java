fdfdf1da400cded63ef5ec8496b358fd66f651df
Harel Aliev <harel.aliev@gmail.com>
2025-12-01 22:23:04 -0500

    Save

--- app/src/main/java/com/example/smartair/rescueInhaler.java
+++ app/src/main/java/com/example/smartair/rescueInhaler.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ChildIconFragment#newInstance} factory method to
+ * Use the {@link rescueInhaler#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ChildIconFragment extends Fragment {
+public class rescueInhaler extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class ChildIconFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ChildIconFragment() {
+    public rescueInhaler() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class ChildIconFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ChildIconFragment.
+     * @return A new instance of fragment rescueInhaler.
      */
     // TODO: Rename and change types and number of parameters
-    public static ChildIconFragment newInstance(String param1, String param2) {
-        ChildIconFragment fragment = new ChildIconFragment();
+    public static rescueInhaler newInstance(String param1, String param2) {
+        rescueInhaler fragment = new rescueInhaler();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class ChildIconFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_child_icon, container, false);
+        return inflater.inflate(R.layout.fragment_rescue_inhaler, container, false);
     }
 }
