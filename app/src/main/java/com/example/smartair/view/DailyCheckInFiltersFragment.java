ee020d907b9b1868d3d909fe2a64182ebd51ec01
Noah Miguel Perez <noahm.p.in2017@gmail.com>
2025-12-02 04:28:42 -0500

    feat: Separated inventory into a rescue and controller side; Added dialogs to update information

--- app/src/main/java/com/example/smartair/view/DailyCheckInFiltersFragment.java
+++ app/src/main/java/com/example/smartair/view/DailyCheckInFiltersFragment.java
@@ -26,67 +26,69 @@ public class DailyCheckInFiltersFragment extends DialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.fragment_filter_daily_check_in_logs, null);
-
-        CheckBox nightWakingCB = view.findViewById(R.id.check_box_night_waking);
-        CheckBox activityLimitsCB = view.findViewById(R.id.check_box_activity_limits);
-        CheckBox otherSymptomCB = view.findViewById(R.id.check_box_other_symptom);
-        CheckBox coughWheezeCB = view.findViewById(R.id.check_box_coughing_wheezing);
-        CheckBox exerciseCB = view.findViewById(R.id.check_box_exercise);
-        CheckBox coldAirCB = view.findViewById(R.id.check_box_cold_air);
-        CheckBox dustPetsCB = view.findViewById(R.id.check_box_dust);
-        CheckBox smokeCB = view.findViewById(R.id.check_box_smoke);
-        CheckBox illnessCB = view.findViewById(R.id.check_box_illness);
-        CheckBox perfumeCB = view.findViewById(R.id.check_box_perfume);
-        CheckBox otherTriggerCB = view.findViewById(R.id.check_box_other_trigger);
-        TextView afterDate = view.findViewById(R.id.edit_text_after_date);
-        TextView beforeDate = view.findViewById(R.id.text_view_before_date);
-
-        afterDate.setOnClickListener(v -> {
-            DatePickerDialog datePicker = new DatePickerDialog(getContext());
-            datePicker.setOnDateSetListener((view1, year, month, dayOfMonth) -> {
-                String dayString = ((dayOfMonth < 10) ? "0" : "") + dayOfMonth;
-                String monthString = ((month < 10) ? "0" : "") + month;
-                afterDate.setText(dayString + "/" + monthString + "/" + year);
-            });
-            datePicker.show();
-        });
-
-        beforeDate.setOnClickListener(v -> {
-            DatePickerDialog datePicker = new DatePickerDialog(getContext());
-            datePicker.setOnDateSetListener((view1, year, month, dayOfMonth) -> {
-                String dayString = ((dayOfMonth < 10) ? "0" : "") + dayOfMonth;
-                String monthString = ((month < 10) ? "0" : "") + month;
-                beforeDate.setText(dayString + "/" + monthString + "/" + year);
-            });
-            datePicker.show();
-        });
-
-        builder.setView(view)
-                .setPositiveButton("Apply filters",
-                        (dialog, which) ->
-                        presenter.applyFiltersButtonClicked(
-                                nightWakingCB.isChecked(),
-                                activityLimitsCB.isChecked(),
-                                coughWheezeCB.isChecked(),
-                                otherSymptomCB.isChecked(),
-                                exerciseCB.isChecked(),
-                                coldAirCB.isChecked(),
-                                smokeCB.isChecked(),
-                                illnessCB.isChecked(),
-                                dustPetsCB.isChecked(),
-                                perfumeCB.isChecked(),
-                                otherTriggerCB.isChecked(),
-                                afterDate.getText().toString(),
-                                beforeDate.getText().toString()
-                        ))
-                .setNegativeButton("Cancel",
-                        (dialog, which) ->
-                        getDialog().cancel());
-
+        setUpInputs(view, builder);
         return builder.create();
     }
 
     public void setPresenter(DailyCheckInLogListPresenter presenter) {
         this.presenter = presenter;
+    }
+
+    public void setUpInputs(View view, AlertDialog.Builder builder) {
+            CheckBox nightWakingCB = view.findViewById(R.id.check_box_night_waking);
+            CheckBox activityLimitsCB = view.findViewById(R.id.check_box_activity_limits);
+            CheckBox otherSymptomCB = view.findViewById(R.id.check_box_other_symptom);
+            CheckBox coughWheezeCB = view.findViewById(R.id.check_box_coughing_wheezing);
+            CheckBox exerciseCB = view.findViewById(R.id.check_box_exercise);
+            CheckBox coldAirCB = view.findViewById(R.id.check_box_cold_air);
+            CheckBox dustPetsCB = view.findViewById(R.id.check_box_dust);
+            CheckBox smokeCB = view.findViewById(R.id.check_box_smoke);
+            CheckBox illnessCB = view.findViewById(R.id.check_box_illness);
+            CheckBox perfumeCB = view.findViewById(R.id.check_box_perfume);
+            CheckBox otherTriggerCB = view.findViewById(R.id.check_box_other_trigger);
+            TextView afterDate = view.findViewById(R.id.edit_text_after_date);
+            TextView beforeDate = view.findViewById(R.id.text_view_before_date);
+
+            afterDate.setOnClickListener(v -> {
+                DatePickerDialog datePicker = new DatePickerDialog(getContext());
+                datePicker.setOnDateSetListener((view1, year, month, dayOfMonth) -> {
+                    String dayString = ((dayOfMonth < 10) ? "0" : "") + dayOfMonth;
+                    String monthString = ((month < 10) ? "0" : "") + month;
+                    afterDate.setText(dayString + "/" + monthString + "/" + year);
+                });
+                datePicker.show();
+            });
+
+            beforeDate.setOnClickListener(v -> {
+                DatePickerDialog datePicker = new DatePickerDialog(getContext());
+                datePicker.setOnDateSetListener((view1, year, month, dayOfMonth) -> {
+                    String dayString = ((dayOfMonth < 10) ? "0" : "") + dayOfMonth;
+                    String monthString = ((month < 10) ? "0" : "") + month;
+                    beforeDate.setText(dayString + "/" + monthString + "/" + year);
+                });
+                datePicker.show();
+            });
+
+            builder.setView(view)
+                    .setPositiveButton("Apply Filters",
+                            (dialog, which) ->
+                                    presenter.applyFiltersButtonClicked(
+                                            nightWakingCB.isChecked(),
+                                            activityLimitsCB.isChecked(),
+                                            coughWheezeCB.isChecked(),
+                                            otherSymptomCB.isChecked(),
+                                            exerciseCB.isChecked(),
+                                            coldAirCB.isChecked(),
+                                            smokeCB.isChecked(),
+                                            illnessCB.isChecked(),
+                                            dustPetsCB.isChecked(),
+                                            perfumeCB.isChecked(),
+                                            otherTriggerCB.isChecked(),
+                                            afterDate.getText().toString(),
+                                            beforeDate.getText().toString()
+                                    ))
+                    .setNegativeButton("Cancel",
+                            (dialog, which) -> getDialog().cancel());
+
     }
 }
---
+++ app/src/main/java/com/example/smartair/view/NewInventoryAmountFragment.java
@@ -0,0 +1,45 @@
+package com.example.smartair.view;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ToggleButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.smartair.R;
+import com.example.smartair.presenter.InventoryPresenter;
+
+public class NewInventoryAmountFragment extends DialogFragment {
+    InventoryPresenter presenter;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.fragment_inventory_new_amount, null);
+        setUpInputs(view, builder);
+        return builder.create();
+    }
+
+    public void setUpInputs(View view, AlertDialog.Builder builder) {
+        ToggleButton targetTB = view.findViewById(R.id.toggle_button_new_amount);
+        EditText newAmount = view.findViewById(R.id.edit_text_new_amount);
+
+        builder.setView(view)
+                .setPositiveButton("Log New Amount", (dialog, which) -> {
+                    presenter.addNewAmount(targetTB.getText().toString(), newAmount.getText().toString());
+                })
+                .setNegativeButton("Cancel", (dialog, which) -> getDialog().cancel());
+    }
+
+    public void setPresenter(InventoryPresenter presenter) {
+        this.presenter = presenter;
+    }
+}
